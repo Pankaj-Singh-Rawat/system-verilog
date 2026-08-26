4f5603f6bcb623eda10ca4a761ee8de3fa5efb33
Pankaj Singh Rawat <qoolphoenix@gmail.com>
2026-08-26 14:57:13 +0530

    mod_n_counter with tb

---
+++ verilog/mod_n_counter.v
@@ -0,0 +1,54 @@
+module mod_n_counter(input wire clk,
+                     input wire rst_n,
+                     input wire enable,
+                     output reg [3:0] count);
+  
+  
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      count <= 4'b0000;
+    end else if (enable) begin
+      if (count == 4'd9) begin
+        count <= 4'b000;
+      end else begin 
+        count <= count + 1'b1;
+      end 
+    end
+  end 
+endmodule
+
+
+
+module tb ();
+
+  reg clk;
+  reg rst_n;
+  reg enable;
+  wire [3:0] count;
+
+  mod_n_counter uut (
+      .clk   (clk),
+      .rst_n (rst_n),
+      .enable(enable),
+      .count (count)
+  );
+
+  always #5 clk = ~clk;
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb); 
+    clk    = 0;
+    rst_n  = 0;
+    enable = 0;
+
+    #12 rst_n = 1;
+    #10 enable = 1;
+    #120;
+
+    $finish;
+  end
+
+endmodule  
+  
+    
